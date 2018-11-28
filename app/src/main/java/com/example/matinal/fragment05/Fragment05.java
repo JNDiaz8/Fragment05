f9aeba0dd54a62ce0a18e1bc7b8401a751e926a2
Joaquin Diaz <nete_8_8_@hotmail.com>
2018-11-28 18:07:58 +0100

    El programa funciona correctamente. Ampliando conocimientos completado en casa.

--- app/src/main/java/com/example/matinal/fragment05/Fragment05.java
+++ app/src/main/java/com/example/matinal/fragment05/Fragment05.java
@@ -2,31 +2,26 @@ package com.example.matinal.fragment05;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class Fragment05 extends AppCompatActivity {
-
-    private Button btnMostrarPreferencias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment05);
 
-
-
     }
-    public void llamada (View view){
+
+    public void mostrarDialogo(View view) {
+
         FragmentManager FM = getFragmentManager();
         FragmentTransaction FT = FM.beginTransaction();
 
         Fragmento1 preferencias = new Fragmento1();
-        FT.replace(android.R.id.content, preferencias);
-        FT.commit();
-
+        FT.add(R.id.contenedor, preferencias).commit();
     }
 }
 
